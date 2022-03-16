5d19d31c6da82d19bce3dfaee0c310ff593aa23e
Noluyolo97 <98300986+Noluyolo97@users.noreply.github.com>
2022-03-16 11:02:05 +0200

    Update 8-complex.c

--- 0x00-math_complex/8-complex.c
+++ 0x00-math_complex/8-complex.c
@@ -1,7 +1,7 @@
 /*
 * 8-complex.c - update complex based on modulus and arguments
-* Author: Rodrigo Zárate Algecira
-* Date: August 03, 2021
+* Author: Noluyolo Ndungane
+* Date: March 16, 2022
 */
 
 #include "holberton.h"
