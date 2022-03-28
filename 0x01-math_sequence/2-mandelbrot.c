b4e90dc125c69f01dbb8628ad04450cea601e9c0
Noluyolo97 <98300986+Noluyolo97@users.noreply.github.com>
2022-03-28 09:57:27 +0200

    Update 2-mandelbrot.c

--- 0x01-math_sequence/2-mandelbrot.c
+++ 0x01-math_sequence/2-mandelbrot.c
@@ -1,56 +1,42 @@
-/*
-* 2-mandelbrot.c - Prints a mandelbrot
-* Author: Noluyolo Ndungane
-* Date: March 23, 2022
-*/
-
 #include <stdio.h>
-
+#include <curses.h>
 /**
-* mandelbrot - do the mandel bro
-* Return: 0
-*/
-
-int mandelbrot(void)
+ * main - Function that prints Mandelbrot’s Sets
+ *
+ * Return: End Program
+ */
+int main(void)
 {
-int A, B, i, z = 100;
-int w, h;
-double a, b, x, y, r;
-/* skew */
-double n = 100;
-/* size of image by 4 */
-FILE *pgmimg;
-w = z * 4, h = z * 4;
-printf("Holberton School\n");
-printf("Mandelbrot's set image created in mandelbrot.pgm file\n");
+	int A, B, i;
+	double a, b, x, y, t, n = 20;
 
-pgmimg = fopen("mandelbrot.pgm", "wb");
-fprintf(pgmimg, "P2\n");
-fprintf(pgmimg, "%d %d\n", w, h);
-fprintf(pgmimg, "255\n");
-for (B = 0; B < 4 * n; B++)
-{
-	b = 2 - (B / n);
-	for (A = 0; A < 4 * n; A++)
+	printf("Mandelbrot Set\n");
+
+	for (B = 0; B <= 4 * n; B++)
 	{
-		a = -2 + (A / n);
-		x = 0;
-		y = 0;
-		for (i = 1; i <= 1024; i++)
+		b = 2 - (B / n);
+		for (A = 0; A <= 4 * n; A++)
 		{
-			r = x;
-			x = (x * x) - (y * y) + a;
-			y = (2 * r * y) + b;
-			if ((x * x) + (y * y) > 4)
-				break;
+			a = -2 + (A / n);
+			x = 0;
+			y = 0;
+
+			for (i = 1; i <= 1000; i++)
+			{
+				t = x;
+				x = (x * x) - (y * y) + a;
+				y = (2 * t * y) + b;
+				if ((x * x) + (y * y) > 4)
+					break;
+			}
+
+			if (i == 1001)
+				printf(".");
+			else
+				printf(" ");
 		}
-		if (i == 1025)
-			fprintf(pgmimg, "255 ");
-		else
-			fprintf(pgmimg, "20 ");
+		printf("\n");
 	}
-fprintf(pgmimg, "\n");
-}
-fclose(pgmimg);
-return (0);
+	getch();
+	return (0);
 }
